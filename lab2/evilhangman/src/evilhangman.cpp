13d57bd365a0c195b650fcd7d13178458e8d7a07
bensu844 <bensu844@tlvm-5-4-1.ad.liu.se>
2021-09-14 10:51:24 +0200

    stolpar

--- lab2/evilhangman/src/evilhangman.cpp
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -8,6 +8,30 @@ int main() {
     cout << "Welcome to Hangman." << endl;
 
     // TODO: Finish the program!
+    // 1. read file
 
+    // 2. prompt word length
+
+    // 3. prompt number of guesses
+
+    // 4. prompt show number of words
+
+    // 5. Actual game
+
+    // a. create list of words based on length
+
+    // b. print remaining guesses + guessed letters (+ remaining words)
+
+    // c. prompt guess
+
+    // d. partition words based on family
+
+    // e. find largest family, ...
+
+    // f. reveal word if out of guesses
+
+    // g. congrats
+
+    // 6. play again?
     return 0;
 }
