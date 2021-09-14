d64bb9ed0247c77306bd3f3565f4a0bf0db65419
bensu844 <bensu844@tlvm-5-4-1.ad.liu.se>
2021-09-14 11:28:14 +0200

    Added variables

--- lab2/evilhangman/src/evilhangman.cpp
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -18,24 +18,54 @@ void getDict(unordered_set<string>& dict){
     file.close();
 }
 
+int getNumberOfGuesses(){
+    cout << "Enter number of guesses:" << endl;
+
+    // add functionality
+}
+
+bool getShowRemainingWords(){
+    cout << "Do you want to see the number of remaining words?" << endl;
+
+   // add functionality
+}
+
 int main() {
+    int remainingGuesses;
+    //int remainingWords;
+    bool showRemainingWords;
+    string wordProgress;
+    string guessedLetters = "";
+    unordered_set<string> possibleWords = {};
+
     cout << "Welcome to Hangman." << endl;
-    unordered_set<string> dict;
-    getDict(dict);
+
+
     // TODO: Finish the program!
     // 1. read file
+    unordered_set<string> dict;
+    getDict(dict);
 
     // 2. prompt word length
 
     // 3. prompt number of guesses
+    remainingGuesses = getNumberOfGuesses();
 
     // 4. prompt show number of words
+    showRemainingWords = getShowRemainingWords();
 
     // 5. Actual game
 
     // a. create list of words based on length
+    // DONE IN 2.
 
     // b. print remaining guesses + guessed letters (+ remaining words)
+    cout << "You have " << remainingGuesses << " guesses left." << endl;
+    cout << "Guessed letters: " << guessedLetters << endl;
+    cout << wordProgress << endl;
+    if (showRemainingWords) {
+        cout << "Remaining words: " << possibleWords.size() << endl;
+    }
 
     // c. prompt guess
 
