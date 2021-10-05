e8cc4a2353ff3af773ba3ff2860f79188ae5b414
bensu844 <bensu844@tlvm-4-2-1.ad.liu.se>
2021-10-05 14:04:49 +0200

    Added header comments

--- lab3/tsp/src/Tour.h
+++ lab3/tsp/src/Tour.h
@@ -1,8 +1,11 @@
-// This is the .h file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own, as well as on the members.
-// TODO: remove this comment header
+/*
+ * TDDD86 TSP
+ * Benjamin Sundvall - bensu844
+ * Anton Wegeström - antwe841
+ *
+ * This file contains the declaration of the Tour structure.
+ * See Tour.cpp for implementation of each member.
+ */
 
 #ifndef TOUR_H
 #define TOUR_H
@@ -16,15 +19,42 @@ public:
     Tour();
     Tour(Point a, Point b, Point c, Point d);
     ~Tour();
+
+    /*
+     * Prints the points that makes up the tour.
+     */
     void show();
+
+    /*
+     * Draws the tour onto the given QGraphicsScene.
+     */
     void draw(QGraphicsScene* scene);
+
+    /*
+     * Returns the number of nodes in the tour.
+     */
     int size();
+
+    /*
+     * Returns the total distance of the tour.
+     */
     double distance();
+
+    /*
+     * Inserts a new node in the tour after the nearest already existing point.
+     */
     void insertNearest(Point p);
+
+    /*
+     * Inserts a new node in the tour in the position that would cause the smallest increase in total distance.
+     */
     void insertSmallest(Point p);
 
 private:
-
+    /*
+     * Returns the increase in distance that would be caused by inserting a new node after the given node.
+     */
+    int getCurrentIncrease(Node* curr, const Point& p);
 };
 
 #endif // TOUR_H
