7125a731ec3c15b21d930915fab4e91dae29841b
bensu844 <bensu844@tlvm-4-1-2.ad.liu.se>
2021-11-30 13:44:59 +0100

    Implemented leftChild(), rightChild() and parent().

--- labb6/MyPriorityQueue.h
+++ labb6/MyPriorityQueue.h
@@ -54,8 +54,7 @@ private:
 
 template <typename T, typename C>
 MyPriorityQueue<T,C>::MyPriorityQueue(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    vector_array = MyVector<T>();
 }
 
 template <typename T, typename C>
@@ -86,6 +85,21 @@ template <typename T, typename C>
 bool MyPriorityQueue<T,C>::empty()const{
     // TODO: replace the code below with your code for this member
     MYEXCEPTION("unimplemented method");
+}
+
+template <typename T, typename C>
+int MyPriorityQueue<T, C>::leftChild(int i){
+    return 2 * i + 1;
+}
+
+template <typename T, typename C>
+int MyPriorityQueue<T, C>::rightChild(int i){
+    return 2 * i + 2;
+}
+
+template <typename T, typename C>
+int MyPriorityQueue<T, C>::parent(int i){
+    return ((i - 1) / 2);
 }
 
 #endif // MY_PRIORITY_QUEUE_H
