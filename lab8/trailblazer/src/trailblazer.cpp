6a3c8ab602480464be16c71437d30c49913f528e
bensu844 <bensu844@su03-103.ad.liu.se>
2021-12-10 16:33:27 +0100

    Implemented DFS

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -8,13 +8,35 @@
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
+vector<Node*> DFS(BasicGraph& graph, Vertex* currentNode, Vertex* targetNode){
+    // Previsit
+    currentNode->visited = true;
+    currentNode->setColor(GREEN);
+    Set<Arc*> outgoingEdges = currentNode->arcs;
+
+    for(Arc* edgeToNeighbor : outgoingEdges){
+        Vertex* neighborNode = edgeToNeighbor->finish;
+
+        if (neighborNode == targetNode) {
+            vector<Node*> toReturn = {currentNode, neighborNode};     // Return the path from current to last (trivial)
+            return toReturn;    // return [currentMode, targetNode]
+        } else if (!neighborNode->visited) {
+            vector<Node*> res = DFS(graph, neighborNode, targetNode);  // Recursive step
+            if(!res.empty()){   // If a path to the target node exists...
+                vector<Node*> toReturn = res;                   // Ugly
+                toReturn.insert(toReturn.begin(), currentNode);
+                return toReturn;    // ... return [currentNode] + res
+            }
+        }
+    }
+    // No path has been found from currentNode to targetNode
+    vector<Node*> toReturn = {};
+    return toReturn;
+}
+
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
-    return path;
+    graph.resetData();
+    return DFS(graph, start, end);
 }
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
