613aa85c8f10efd4fe89be6698d01853bacf267c
bensu844 <bensu844@humanoid1-301.ad.liu.se>
2021-12-14 11:03:08 +0100

    Fixed dijkstra and a*

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -49,74 +49,6 @@ vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     return DFS(graph, start, end);
 }
 
-vector<Node*> BFS(BasicGraph& graph, Vertex* currentNode, Vertex* targetNode, Vector<Vertex*> path){
-    /*
-    // Previsit
-    currentNode->visited = true;
-    currentNode->setColor(GREEN);
-    Set<Arc*> outgoingEdges = currentNode->arcs;
-
-    for(Arc* edgeToNeighbor : outgoingEdges){
-        Vertex* neighborNode = edgeToNeighbor->finish;
-
-        if (neighborNode == targetNode) {
-            //vector<Node*> toReturn = {currentNode, neighborNode};     // Return the path from current to last (trivial)
-            //return toReturn;    // return [currentMode, targetNode]
-        } else if (!neighborNode->visited) {
-            //vector<Node*> res = DFS(graph, neighborNode, targetNode);  // Recursive step
-            //if(!res.empty()){   // If a path to the target node exists...
-                //vector<Node*> toReturn = res;                   // Ugly
-                //toReturn.insert(toReturn.begin(), currentNode);
-                //return toReturn;    // ... return [currentNode] + res
-            }
-        }
-    }
-
-    return BFS(graph, )
-    // No path has been found from currentNode to targetNode
-    vector<Node*> toReturn = {};
-    return toReturn;
-    */
-}
-
-/* Senaste
-vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* startNode, Vertex* targetNode) {
-    graph.resetData();
-    queue<vector<Vertex*>> searchQueue;
-    vector<Vertex*> path = {startNode};
-    searchQueue.push(path);
-
-    while(!searchQueue.empty()){
-        vector<Vertex*> path = searchQueue.front();     // continue on the next potential path
-        searchQueue.pop();
-
-        Vertex* currentNode = path.back();
-        currentNode->visited = true;
-        currentNode->setColor(GREEN);
-
-        // check if current path leads to the target node
-        if(currentNode == targetNode){
-            return path;
-        }
-
-        // queue exploration of all neigbors of the current node
-        for(Arc* edgeToNeighbor : currentNode->arcs){
-            Vertex* neighbor = edgeToNeighbor->finish;
-            if(!neighbor->visited){
-                vector<Vertex*> newPath = path;
-                newPath.push_back(neighbor);
-                neighbor->setColor(YELLOW);
-                searchQueue.push(newPath);
-            }
-        }
-    }
-    path.clear();   // No path to target found
-    return path;
-}
- */
-
-
-
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* startNode, Vertex* targetNode) {
     graph.resetData();
     queue<Vertex*> searchQueue;
@@ -190,37 +122,36 @@ vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end)
         Node* shortestDistNode = priorityQueue.dequeue();
         shortestDistNode->setColor(GREEN);
 
+        if (shortestDistNode == end){
+            // Update node
+            path.push_back(shortestDistNode);
+            // Add all previous nodes to path
+            while (shortestDistNode != start) {
+                path.push_back(shortestDistNode->previous);
+                shortestDistNode = shortestDistNode->previous;
+            }
+            // Reverse and return
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
         for (Arc* edgeToNeighbor : shortestDistNode->arcs){
             Vertex* neighborNode = edgeToNeighbor->finish;
             // If neighbor is end node
-            if (neighborNode == end){
-                // Update node
+
+            double newCost = shortestDistNode->cost + edgeToNeighbor->cost;
+            // If new cost lower, update cost
+            if (newCost < neighborNode->cost){
+                neighborNode->cost = newCost;
                 neighborNode->previous = shortestDistNode;
-                path.push_back(neighborNode);
-                neighborNode->setColor(GREEN);
-                // Add all previous nodes to path
-                while (neighborNode != start) {
-                    path.push_back(neighborNode->previous);
-                    neighborNode = neighborNode->previous;
-                }
-                // Reverse and return
-                std::reverse(path.begin(), path.end());
-                return path;
-            }
-            else {
-                double newCost = shortestDistNode->cost + edgeToNeighbor->cost;
-                // If new cost lower, update cost
-                if (newCost < neighborNode->cost){
-                    neighborNode->cost = newCost;
-                    neighborNode->previous = shortestDistNode;
-                    priorityQueue.changePriority(neighborNode, newCost);
-                    if (!neighborNode->visited){
-                        // Update node
-                        neighborNode->visited = true;
-                        neighborNode->setColor(YELLOW);
-                    }
+                priorityQueue.changePriority(neighborNode, newCost);
+                if (!neighborNode->visited){
+                    // Update node
+                    neighborNode->visited = true;
+                    neighborNode->setColor(YELLOW);
                 }
             }
+
         }
     }
     return path;
@@ -244,38 +175,36 @@ vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
         Node* shortestDistNode = priorityQueue.dequeue();
         shortestDistNode->setColor(GREEN);
 
+        if (shortestDistNode == end){
+            // Update node
+            path.push_back(shortestDistNode);
+            // Add all previous nodes to path
+            while (shortestDistNode != start) {
+                path.push_back(shortestDistNode->previous);
+                shortestDistNode = shortestDistNode->previous;
+            }
+            // Reverse and return
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
         for (Arc* edgeToNeighbor : shortestDistNode->arcs){
             Vertex* neighborNode = edgeToNeighbor->finish;
             // If neighbor is end node
-            if (neighborNode == end){
-                // Update node
+
+            double newCost = shortestDistNode->cost + edgeToNeighbor->cost;
+            // If new cost lower, update cost
+            if (newCost < neighborNode->cost){
+                neighborNode->cost = newCost;
                 neighborNode->previous = shortestDistNode;
-                path.push_back(neighborNode);
-                neighborNode->setColor(GREEN);
-                // Add all previous nodes to path
-                while (neighborNode != start) {
-                    path.push_back(neighborNode->previous);
-                    neighborNode = neighborNode->previous;
+                priorityQueue.changePriority(neighborNode, newCost + neighborNode->heuristic(end));
+                if (!neighborNode->visited){
+                    // Update node
+                    neighborNode->visited = true;
+                    neighborNode->setColor(YELLOW);
                 }
-                // Reverse and return
-                std::reverse(path.begin(), path.end());
-                return path;
             }
 
-            else {
-                double newCost = shortestDistNode->cost + edgeToNeighbor->cost;
-                // If new cost lower, update cost
-                if (newCost < neighborNode->cost){
-                    neighborNode->cost = newCost;
-                    neighborNode->previous = shortestDistNode;
-                    priorityQueue.changePriority(neighborNode, newCost + neighborNode->heuristic(end));
-                    if (!neighborNode->visited){
-                        // Update node
-                        neighborNode->visited = true;
-                        neighborNode->setColor(YELLOW);
-                    }
-                }
-            }
         }
     }
     return path;
