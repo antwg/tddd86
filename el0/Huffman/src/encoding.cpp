5da0bd4abbe18bee4b80435b6297931bbc8fcd17
antwe841 <antwe841@tlvm-4-4-3.ad.liu.se>
2021-12-20 18:23:27 +0100

    Trasig buildEncodingTree

--- el0/Huffman/src/encoding.cpp
+++ el0/Huffman/src/encoding.cpp
@@ -7,6 +7,8 @@
 
 #include "encoding.h"
 #include <iterator>
+#include <queue>
+#include <vector>
 // TODO: include any other headers you need
 
 using namespace std;
@@ -28,15 +30,38 @@ map<int, int> buildFrequencyTable(istream& input) {
         // Get next letter
         character = input.get();
     }
-
-
-
     return freqTable;
 }
 
+bool compare(HuffmanNode l, HuffmanNode r){
+    return l.count > r.count;
+}
+
 HuffmanNode* buildEncodingTree(const map<int, int> &freqTable) {
-    // TODO: implement this function
-    return nullptr;
+    // TODO Sorterar efter address, inte count, av nån jävla anledning
+    std::priority_queue<HuffmanNode> queue;
+    // Add all chars to queue as nodes
+    for (auto it = freqTable.begin(); it != freqTable.end(); it++){
+        HuffmanNode node = HuffmanNode(it->first, it->second, nullptr, nullptr);
+        queue.push(node);
+    }
+    while (queue.size() >= 2){
+        // Get two smallest
+        HuffmanNode leftChild = queue.top();
+        HuffmanNode* leftChildptr = &leftChild;
+        queue.pop();
+
+        HuffmanNode rightChild = queue.top();
+        HuffmanNode* rightChildptr = &rightChild;
+        queue.pop();
+
+        // Create new node with two smallest as children
+        HuffmanNode newNode = HuffmanNode(NOT_A_CHAR, leftChild.count + rightChild.count, leftChildptr, rightChildptr);
+        queue.push(newNode);
+    }
+    HuffmanNode root = queue.top();
+    HuffmanNode* rootptr = & root;
+    return rootptr;
 }
 
 map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
